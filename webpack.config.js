c8d02f5275df1d9221af5c819e0392f75a7fb158
Tim Heyer <theyer3@gmail.com>
2020-08-17 20:12:37 -0500

    [WIP] Client-side changes to talk to a REST backend.
    A lot of this may go away as I'm going to throw away my half-built Flask+Mongo backend in favor of Firebase/Firestore.

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
   },
   devServer: {
     contentBase: path.join(__dirname, "/"),
+    disableHostCheck: true,
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
     hotOnly: true
